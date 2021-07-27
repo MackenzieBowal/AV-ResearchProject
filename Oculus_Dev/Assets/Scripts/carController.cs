087ead96191eb707606b1291d3d3492f22340bff
weiwei <rerenaissance@outlook.com>
2021-07-26 21:25:46 -0600

    finished all the animation and counterbalanced order

--- Oculus_Dev/Assets/Scripts/carController.cs
+++ Oculus_Dev/Assets/Scripts/carController.cs
@@ -33,6 +33,8 @@ public class carController : MonoBehaviour
     private GameObject[] lips = new GameObject[3];
     private GameObject rightEye;
     private GameObject leftEye;
+    private Quaternion rEyeRot;
+    private Quaternion lEyeRot;
     private string[] armAnimations = { "Go Ahead", "Still Palm Out", "Forearm Wave", "Finger Point", "Motion Downward", "One Finger Wait" };
     private string[] batonArmAnimations = { "Sweep Sideways", "Baton Circles" };
     private GameObject hand;
@@ -45,11 +47,12 @@ public class carController : MonoBehaviour
     //faceVelocity controls the speed of face showup, using localposition
     private Vector3 faceVelocity = new Vector3(0.0f, 0.0f, 0.219f);
     //It will take 3 second for the vehicle to totally stop, hence the deceleration is 3.3 m/s^2
-    private Vector3 deceleration = new Vector3(0.0f, 0.0f, - 3.15f);
+    private Vector3 deceleration = new Vector3(0.0f, 0.0f, - 3.24f);
     //The total number of all tasks for one participant
-    private const int TOTAL_TASK_NUM = 23;
+    //private const int TOTAL_TASK_NUM = 44;
+    private const int MODE_NUM = 22;
     //The task that will be showed next
-    private int taskNum = 0;
+    private int taskNum = -1;
     private int[] randomOrder;
     private int userID;
     private int coachmanType = -1;
@@ -104,7 +107,6 @@ public class carController : MonoBehaviour
         userCollection = database.GetCollection<BsonDocument>("userinfos");
         //Get and update the userID
         GetAndUpdateUserID();
-        Debug.Log("the value randomOrder[0]" + randomOrder[0]);
         SetAnimator();
         Debug.Log("start function is over");
     }
@@ -136,7 +138,8 @@ public class carController : MonoBehaviour
         if(isStart)
         {  
             //moving AV
-            this.transform.Translate(velocity * deltaTime);
+            if(velocity.z >= 0)
+                this.transform.Translate(velocity * deltaTime);
             //Slow down
             if (!isSlowDown&&this.transform.position.z > -16 )
             {
@@ -147,7 +150,6 @@ public class carController : MonoBehaviour
                 {
                     //play animation
                     animationFlag = true;
-                    velocity.z = 0;
                     isSlowDown = true;
                     PlayAnimation();
                 }
@@ -160,7 +162,7 @@ public class carController : MonoBehaviour
         }
         if(isRestart)
         {
-            if(velocity.z < 350/36)
+            if(velocity.z <= 350/36)
             {
                 velocity = velocity - deceleration * deltaTime;
                 //hide interfaces
@@ -168,10 +170,7 @@ public class carController : MonoBehaviour
             }
             this.transform.Translate(velocity * deltaTime);
             if(!isEndTask&&this.transform.position.z > 19)
-            {
-                menu.SetActive(true);
-                isEndTask = true;
-            }
+                EndTask();
             isRestart = this.transform.position.z < 49;
         }
         if(isBlocked&&centerEye.transform.position.x > -320)
@@ -183,10 +182,10 @@ public class carController : MonoBehaviour
 
     public void MoveInterfaces(bool isPop, float deltaTime)
     {
-        int pos = randomOrder[taskNum] % 23;
+        int pos = randomOrder[taskNum] % MODE_NUM;
         float weight = isPop? 1: -1;
-        string armAniName = isPop? "Open": "Start";
-        string batonArmAniName = isPop? "OpenBaton": "StartBaton";
+        string armAniName = isPop? "Open": "Close";
+        string batonArmAniName = isPop? "OpenBaton": "CloseBaton";
         //pop up
         if(coachmanType <= 3 && coachmanType >= 0)
             {
@@ -194,16 +193,16 @@ public class carController : MonoBehaviour
                 coachmen[coachmanType].transform.Translate(new Vector3(0f, weight*popDis[coachmanType] / 3.0f, 0f) * deltaTime);
             }
         //hand gestures
-        if ( pos >= 16 && pos <= 21)
+        if ( pos >= 16 && pos <= MODE_NUM - 2)
             activatedAnimator.Play(armAniName);
-        else if (pos == 22 && pos == 0)
+        else if (pos == MODE_NUM-1 || pos == 0)
             activatedAnimator.Play(batonArmAniName);
         else if (pos >= 10 && pos <= 15)
             {
-                rightEye.transform.Translate(weight*faceVelocity * deltaTime);
-                leftEye.transform.Translate(weight*faceVelocity * deltaTime);
+                rightEye.transform.Translate(weight*faceVelocity * deltaTime, Space.World);
+                leftEye.transform.Translate(weight*faceVelocity * deltaTime, Space.World);
                 lips[0].transform.Translate(weight*faceVelocity * deltaTime, Space.World);
-                lips[1].transform.Translate(weight*faceVelocity * deltaTime,Space.World);
+                lips[1].transform.Translate(weight*faceVelocity * deltaTime, Space.World);
                 lips[2].transform.Translate(weight*faceVelocity * deltaTime, Space.World);
             }
     }
@@ -213,7 +212,7 @@ public class carController : MonoBehaviour
     */
     public void PlayAnimation()
     {
-        int pos = randomOrder[taskNum] % 23;
+        int pos = randomOrder[taskNum] % MODE_NUM;
         if (1 < pos&&pos < 10)
         {
             //coachman design
@@ -229,11 +228,11 @@ public class carController : MonoBehaviour
                 isEyeTrack = false;
         }
         //no baton hand gestures
-        else if (pos >= 16 && pos <= 21)
+        else if (pos >= 16 && pos <= MODE_NUM-2)
         {
             activatedAnimator.Play(armAnimations[pos - 16]);
         }
-        else if (pos == 22)
+        else if (pos == MODE_NUM-1)
         {
             activatedAnimator.Play(batonArmAnimations[0]);
         }
@@ -244,7 +243,7 @@ public class carController : MonoBehaviour
     {
         taskNum++;
         //Debug.Log(taskNum);
-        if (taskNum >= TOTAL_TASK_NUM)
+        if (taskNum >= 2*MODE_NUM)
             {
                 Debug.Log("#####Experiment is over");
                 quitButton.SetActive(true);
@@ -261,7 +260,7 @@ public class carController : MonoBehaviour
             isEndTask = false;
             velocity = new Vector3(0.0f, 0.0f, 350/36f);
             startTime = Time.realtimeSinceStartup;
-            this.transform.localPosition = new Vector3(-322.0f, 71.7f, -21.0f);  
+            this.transform.localPosition = new Vector3(-322.0f, 71.7f, -21.0f);
             SendDataToDB();
         }
     }
@@ -281,7 +280,7 @@ public class carController : MonoBehaviour
     
     public void TriggerSeeArounnd()
     {
-        activatedAnimator.Play("Idle");
+        activatedAnimator.Play("SeeAround" + coachmanTag[coachmanType]);
         activatedAnimator.Update(0);
     }
     
@@ -344,8 +343,9 @@ public class carController : MonoBehaviour
         var update = Builders<BsonDocument>.Update.Set("userID", userID + 1);
         await userCollection.UpdateOneAsync(filter, update);
         //Generate the order for each participant and save it to database
-        randomOrder = GetRandomList(TOTAL_TASK_NUM);
-        SaveOrderInfoToDB();
+        //randomOrder = GetRandomList(TOTAL_TASK_NUM);
+        randomOrder = GetCounterBalancedOrder();
+        //SaveOrderInfoToDB();
     }
     
     public void timer()
@@ -404,17 +404,41 @@ public class carController : MonoBehaviour
         return ans;
     }
 
+    public int[] GetCounterBalancedOrder()
+    {
+        //most stupid hard code, because I don't wanna read file :(
+        int[,] balancedOrder = 
+        {{1,22,2,21,3,20,4,19,5,18,6,17,7,16,8,15,9,14,10,13,11,12,2,1,3,22,4,21,5,20,6,19,7,18,8,17,9,16,10,15,11,14,12,13},
+        {3,2,4,1,5,22,6,21,7,20,8,19,9,18,10,17,11,16,12,15,13,14,4,3,5,2,6,1,7,22,8,21,9,20,10,19,11,18,12,17,13,16,14,15},
+        {5,4,6,3,7,2,8,1,9,22,10,21,11,20,12,19,13,18,14,17,15,16,6,5,7,4,8,3,9,2,10,1,11,22,12,21,13,20,14,19,15,18,16,17},
+        {7,6,8,5,9,4,10,3,11,2,12,1,13,22,14,21,15,20,16,19,17,18,8,7,9,6,10,5,11,4,12,3,13,2,14,1,15,22,16,21,17,20,18,19},
+        {9,8,10,7,11,6,12,5,13,4,14,3,15,2,16,1,17,22,18,21,19,20,10,9,11,8,12,7,13,6,14,5,15,4,16,3,17,2,18,1,19,22,20,21},
+        {11,10,12,9,13,8,14,7,15,6,16,5,17,4,18,3,19,2,20,1,21,22,12,11,13,10,14,9,15,8,16,7,17,6,18,5,19,4,20,3,21,2,22,1},
+        {13,12,14,11,15,10,16,9,17,8,18,7,19,6,20,5,21,4,22,3,1,2,14,13,15,12,16,11,17,10,18,9,19,8,20,7,21,6,22,5,1,4,2,3},
+        {15,14,16,13,17,12,18,11,19,10,20,9,21,8,22,7,1,6,2,5,3,4,16,15,17,14,18,13,19,12,20,11,21,10,22,9,1,8,2,7,3,6,4,5},
+        {17,16,18,15,19,14,20,13,21,12,22,11,1,10,2,9,3,8,4,7,5,6,18,17,19,16,20,15,21,14,22,13,1,12,2,11,3,10,4,9,5,8,6,7},
+        {19,18,20,17,21,16,22,15,1,14,2,13,3,12,4,11,5,10,6,9,7,8,20,19,21,18,22,17,1,16,2,15,3,14,4,13,5,12,6,11,7,10,8,9},
+        {21,20,22,19,1,18,2,17,3,16,4,15,5,14,6,13,7,12,8,11,9,10,22,21,1,20,2,19,3,18,4,17,5,16,6,15,7,14,8,13,9,12,10,11}
+        };
+        int[] ans = new int[MODE_NUM*2];
+        for(int i = 0; i <  MODE_NUM*2; i++)
+        {
+            ans[i] = balancedOrder[userID-1,i];
+        }
+        return ans;
+    }
+
     public void SetAnimator()
     {
-        int pos = randomOrder[taskNum] % 23;
+        int pos = randomOrder[taskNum] % MODE_NUM;
         Debug.Log("pos is :" + pos);
         rightEye.SetActive(pos >= 10 && pos <= 15);
         leftEye.SetActive(pos >= 10 && pos <= 15);
         lips[0].SetActive(pos == 10 || pos == 13);
         lips[1].SetActive(pos == 11 || pos == 14);
         lips[2].SetActive(pos == 12 || pos == 15);
-        hand.SetActive(pos >= 16 && pos <= 21);
-        handBaton.SetActive(pos == 22 || pos == 23);
+        hand.SetActive(pos >= 16 && pos <= MODE_NUM -2 );
+        handBaton.SetActive(pos == 0 || pos == MODE_NUM-1);
 
         //disable all unchosen coachman
         coachmanType = pos / 2 - 1;
@@ -430,12 +454,12 @@ public class carController : MonoBehaviour
             activatedAnimator = capsuleCoachman1Animator;
         else if (coachmanType == 3)
             activatedAnimator = capsuleCoachman2Animator;
-        else if (pos >= 16 && pos <= 21)
+        else if (pos >= 16 && pos <= MODE_NUM-2)
         {
             activatedAnimator = handAnimator;
             activatedAnimator.Play("Start");
         }
-        else if (pos >= 22 && pos <= 23)
+        else if (pos == MODE_NUM -1 || pos == 0)
         {
             activatedAnimator = handBatonAnimator;
             activatedAnimator.Play("StartBaton");
@@ -458,6 +482,24 @@ public class carController : MonoBehaviour
         {
             isRestart = true;
         }
+    }
+
+    public void EndTask()
+    {
+        menu.SetActive(true);
+        isEndTask = true;
+        isEyeTrack = false;
+        coachmen[0].transform.localPosition = new Vector3(0.51f, -1.35f,-0.4f);
+        coachmen[1].transform.localPosition = new Vector3(0.5f, -2f,-0.45f);
+        coachmen[2].transform.localPosition = new Vector3(0.5f, -1.7f,-0.42f);
+        coachmen[3].transform.localPosition = new Vector3(0.5f, -1.38f,-0.42f);
+        lips[0].transform.localPosition = new Vector3(-0.006f, 0.35f,-0.95f);
+        lips[1].transform.localPosition = new Vector3(0.01f, 0.28f,-0.95f);
+        lips[2].transform.localPosition = new Vector3(-0.012f, 0.438f,-0.995f);
+        rightEye.transform.localPosition = new Vector3(-0.51f, 0.65f, -0.7f);
+        leftEye.transform.localPosition = new Vector3(0.51f, 0.65f, -0.7f);
+        rightEye.transform.localEulerAngles = new Vector3(0f, -180f, 0f);
+        leftEye.transform.localEulerAngles = new Vector3(0f, -180f, 0f);
     }
 
     //This method will not be called
